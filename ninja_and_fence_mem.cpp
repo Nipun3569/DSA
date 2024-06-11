44dd05c9aacd6b458813a2eb240eb68e49ba2087
Nipun3569 <95686790+Nipun3569@users.noreply.github.com>
2024-06-11 14:13:37 +0530

    Create ninja_and_fence_mem.cpp

---
+++ ninja_and_fence_mem.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define MOD 1000000007
+// since we have to do modulo
+int add(int a,int b){
+    return (a%MOD + b%MOD)%MOD;
+}
+
+int mul(int a,int b){
+    return ( (a%MOD) * (b%MOD) )%MOD;
+}
+
+
+int solveMem(int n,int k,vector<int> dp){
+    if(n==1){
+        return k;
+    }
+    if(n==2){
+        return add(k,mul(k,k-1));
+
+    }
+    
+    if(dp[n]!=-1){
+        return dp[n];
+    }
+
+    dp[n]=add(mul(solveMem(n-2,k,dp),k-1), mul(solveMem(n-1,k,dp),k-1));
+    return dp[n];
+}
+int numberOfWays(int n, int k) {
+    
+   vector<int> dp(n+1,-1);
+   return solveMem(n,k,dp);
+}
