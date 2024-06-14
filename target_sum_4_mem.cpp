b6ee4582479ecc4f246faacb8abbfe25f7b5da3c
Nipun3569 <95686790+Nipun3569@users.noreply.github.com>
2024-06-14 11:23:23 +0530

    Create target_sum_4_mem.cpp

---
+++ target_sum_4_mem.cpp
@@ -0,0 +1,23 @@
+int solveMem(vector<int> &num,int tar,vector<int> &dp){
+    //base case
+    if(tar<0){
+        return 0;
+    }
+    if(tar==0){
+        return 1;
+    }
+    if(dp[tar]!=-1){
+        return dp[tar];
+    }
+    int ans=0;
+    for(int i=0;i<num.size();i++){
+        ans+=solveMem(num,tar-num[i],dp);
+    }
+    dp[tar]=ans;
+    return dp[tar];
+}
+int findWays(vector<int> &num, int tar)
+{
+    vector<int> dp(tar+1,-1);
+    return solveMem(num,tar,dp);
+}
