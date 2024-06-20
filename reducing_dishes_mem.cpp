08aa84def4c4e8216c38bacda296d5a93821c8a2
Nipun3569 <95686790+Nipun3569@users.noreply.github.com>
2024-06-20 13:46:26 +0530

    Create reducing_dishes_mem.cpp

---
+++ reducing_dishes_mem.cpp
@@ -0,0 +1,21 @@
+int solveMem(vector<int>& satisfaction,int index,int time,vector<vector<int>> & dp){
+        //base case
+        if(index==satisfaction.size()){
+            return 0;
+        }
+        if(dp[index][time]!=-1){
+            return dp[index][time];
+        }
+        int include=satisfaction[index]*(time+1)+ solveMem(satisfaction,index+1,time+1,dp);
+        int exclude=0+solveMem(satisfaction,index+1,time,dp);
+        dp[index][time]=max(include,exclude);
+        return dp[index][time];
+    }
+    int maxSatisfaction(vector<int>& satisfaction) {
+        //sort
+        sort(satisfaction.begin(),satisfaction.end());
+        int n=satisfaction.size();
+        vector<vector<int>> dp(n+1,vector<int> (n+1,-1));
+        return solveMem(satisfaction,0,0,dp);
+    }
+};
