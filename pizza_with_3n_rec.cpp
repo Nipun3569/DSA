6bec8967dafe37cbdca04778f5b02e60ec1ab775
Nipun3569 <95686790+Nipun3569@users.noreply.github.com>
2024-06-22 14:05:39 +0530

    Update pizza_with_3n_rec.cpp

--- pizza_with_3n_rec.cpp
+++ pizza_with_3n_rec.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    int solve(int index,vector<int> &slices,int n){
+    int solve(int index,int endIndex,vector<int> &slices,int n){
         //base case
-        if(n==0 || index>=slices.size()){
+        if(n==0 || index>endIndex){
             return 0;
         }
-        int take=slices[index]+solve(index+2,slices,n-1);
-        int nottake=0+solve(index+1,slices,n);
+        int take=slices[index]+solve(index+2,endIndex,slices,n-1);
+        int nottake=0+solve(index+1,endIndex,slices,n);
         return max(take,nottake);
 
 
@@ -14,8 +14,8 @@ public:
     int maxSizeSlices(vector<int>& slices) {
         
         int k=slices.size();
-        int case1=solve(0,slices,k/3);
-        int case2=solve(1,slices,k/3);
+        int case1=solve(0,k-2,slices,k/3);
+        int case2=solve(1,k-1,slices,k/3);
         return max(case1,case2);
     }
 };
