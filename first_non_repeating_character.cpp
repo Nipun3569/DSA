99799f28c369a08457a117586d0fa16d5df76dc6
Nipun3569 <95686790+Nipun3569@users.noreply.github.com>
2024-07-06 17:45:30 +0530

    Create first_non_repeating_character.cpp

---
+++ first_non_repeating_character.cpp
@@ -0,0 +1,32 @@
+class Solution {
+	public:
+		string FirstNonRepeating(string A){
+		    map<char, int> m;
+		    string ans = "";
+		    queue<char> q;
+		    
+		    for(int i=0; i<A.length(); i++) {
+		        char ch = A[i];
+		        
+		        q.push(ch);
+		        m[ch]++;
+		        
+		        while(!q.empty()) {
+		            if(m[q.front()] > 1){
+		                //repeating character
+		                q.pop();
+		            }
+		            else
+		            {//non reapeating character
+		                ans.push_back(q.front());
+		                break;
+		            }
+		        }
+		        if(q.empty()){
+		            ans.push_back('#');
+		        }
+		    }
+		    return ans;
+		}
+
+};
